66c75348287847f946fec87b445e04177d722452
gvettoretti <giacomo_vettoretti@outlook.com>
2020-01-17 17:29:22 +0100

    aggiornamento interfaccia component

--- Component.cpp
+++ Component.cpp
@@ -3,3 +3,20 @@
 //
 
 #include "Component.h"
+
+Component::Component() {
+
+}
+
+Component::Component(int id, std::string name, int month, int price_1, int price_2, int price_3) {
+
+}
+
+int Component::getMonth() const {
+    return 0;
+}
+
+int Component::getPrice(int quantity) const {
+    return 0;
+}
+
--- Component.h
+++ Component.h
@@ -6,7 +6,22 @@
 #define INC_02_APPROVVIGIONAMENTO_COMPONENT_H
 
 
-class Component {
+#include "Entity.h"
+#include <string>
+
+class Component : public Entity{
+    int month;
+    int price_1; //price per quantita<11
+    int price_2; //price per quantita 10<quantità<50
+    int price_3; //price per quantita 51+
+
+
+public:
+    Component();
+    Component(int id, std::string name, int month, int price_1, int price_2 , int price_3 );
+
+    int getMonth() const;
+    int getPrice(int quantity) const;
 
 };
 
